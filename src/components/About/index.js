a427e148d8042a7888d8f43ff9458c7d3b210124
Ellie Sargent <elliecsargent@gmail.com>
2022-09-03 10:46:28 -0500

    added Nav component

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function About() {
+    return (
+        <section>
+            <h1 id='about'>About Me!</h1>
+            <div>
+                <p>
+                    This is where I will write about me!
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default About;
