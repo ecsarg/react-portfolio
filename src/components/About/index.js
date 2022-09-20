bdfa2b32fdc08332a105639aa0a6b592abe15e54
ecsarg <100548768+ecsarg@users.noreply.github.com>
2022-09-19 20:48:57 -0500

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,13 +8,7 @@ function About() {
         <img src={aboutImage} className="my-2" style={{ width: "100%" }} alt="cover" />
         <div className="my-2">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
-            condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
-            mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum.
-            Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique
-            justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea
-            dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh.
-            Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+            I'm a recent graduate of the Vanderbilt University Coding Bootcamp looking for employment within the developer field. I got my start in technology while getting my undergrad degree at Auburn University working for the Academic Support office as their technology manager. I then pursued my master's degree in Occupational Therapy. My heart was always pulling me back to technology so I decided to take the plunge and go back to school and I am so glad I did. Enjoy browing some of my portfolio projects. In the footer below you can access my GitHub and LinkedIn.
           </p>
         </div>
       </section>
