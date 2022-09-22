3a213ad8f607c4097aaa05543c0436adc68bd647
Ellie Sargent <elliecsargent@gmail.com>
2022-09-22 18:31:00 -0500

    fixed portfolio cards

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -9,61 +9,61 @@ import WorkoutTracker from '../../assets/workout-tracker.png';
 function Portfolio() {
   return (
     <section className='portfolio'>
-      <div className='row row-cols-1 row-cols-sm-2 rows-cols-md-2 rows-cols-lg-3 g-4'>
-        <div className='col'>
-          <div className='card'>
+      <div className='row'>
+        <div className='col-sm-6 col-md-3 col-lg-3'>
+          <div>
             <img src={RunBuddy} alt='Run Buddy' className='card-img-top' />
-            <div className='card-body'>
+            <div className='card-body text-center'>
               <a href='https://ecsarg.githubio/run-buddy' target='blank' rel='noreferrer'>
                 <button>Run Buddy</button>
               </a>
             </div>
           </div>
         </div>
-        <div className='col'>
-          <div className='card'>
+        <div className='col-sm-6 col-md-3 col-lg-3'>
+          <div>
             <img src={PasswordGenerator} alt='Password Generator' className='card-img-top' />
-            <div className='card-body'>
+            <div className='card-body text-center'>
               <a href='https://ecsarg.github.io/password-generator/' target='blank' rel='noreferrer'>
                 <button>Password Generator</button>
               </a>
             </div>
           </div>
         </div>
-        <div className='col'>
-          <div className='card'>
+        <div className='col-sm-6 col-md-3 col-lg-3'>
+          <div>
             <img src={DogAdoption} alt='Dog Adoption Assistant' className='card-img-top' />
-            <div className='card-body'>
+            <div className='card-body text-center'>
               <a href='https://westsideriley.github.io/Group-Project-1/' target='blank' rel='noreferrer'>
                 <button>Dog Adoption Assistant</button>
               </a>
             </div>
           </div>
         </div>
-        <div className='col'>
-          <div className='card'>
+        <div className='col-sm-6 col-md-3 col-lg-3'>
+          <div>
             <img src={RecordSwap} alt='The Record Swap' className='card-img-top' />
-            <div className='card-body'>
+            <div className='card-body text-center'>
               <a href='https://gentle-stream-19606.herokuapp.com/' target='blank' rel='noreferrer'>
                 <button>The Record Swap</button>
               </a>
             </div>
           </div>
         </div>
-        <div className='col'>
-          <div className='card'>
+        <div className='col-sm-6 col-md-3 col-lg-3'>
+          <div>
             <img src={FantasticUmbrella} alt='Fantastic Umbrella' className='card-img-top' />
-            <div className='card-body'>
+            <div className='card-body text-center'>
               <a href='https://github.com/ecsarg/fantastic-umbrella' target='blank' rel='noreferrer'>
                 <button>E-commerce: Fantastic Umbrella</button>
               </a>
             </div>
           </div>
         </div>
-        <div className='col'>
-          <div className='card'>
+        <div className='col-sm-6 col-md-3 col-lg-3'>
+          <div>
             <img src={WorkoutTracker} alt='Workout Tracker' className='card-img-top' />
-            <div className='card-body'>
+            <div className='card-body text-center'>
               <a href='https://infinite-falls-51606.herokuapp.com/' target='blank' rel='noreferrer'>
                 <button>Workout Tracker</button>
               </a>
